f720467afcbc7e8ed74244d24a94378401308e33
Claire <68912730+clairegll@users.noreply.github.com>
2023-12-07 18:10:32 +0100

    shopify connector teest

--- shopify_connector/models/models.py
+++ shopify_connector/models/models.py
@@ -4,11 +4,4 @@ from odoo import models, fields, api
 
 class shopify_connector(models.Model):
     _name = 'shopify_connector.shopify_connector'
-
-    name = fields.Char()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+    name = fields.Char(string="Title", required=True)description = fields.Text()
