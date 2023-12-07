7b9c72d051c3a7ab616b01a6b08318a977e3c4e7
Claire <68912730+clairegll@users.noreply.github.com>
2023-12-07 18:46:50 +0100

    shopify connector ça rend fou

--- shopify_connector/models/models.py
+++ shopify_connector/models/models.py
@@ -4,4 +4,12 @@ from odoo import models, fields, api
 
 class shopify_connector(models.Model):
     _name = 'shopify_connector.shopify_connector'
-    name = fields.Char(string="Title", required=True)description = fields.Text()
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
